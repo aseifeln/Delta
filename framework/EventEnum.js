9443dff5ab8064df2f601d0f7c7bd40a92470828
Ash-117 <32125521+Ash-117@users.noreply.github.com>
2017-11-06 10:49:30 -0800

    Update EventEnum.js

--- framework/EventEnum.js
+++ framework/EventEnum.js
@@ -22,7 +22,6 @@ const EventEnum = {
 	keydown_g: "keydown_g",
 	keydown_w: "keydown_w",
 	keydown_s: "keydown_s",
-	keydown_g: "keydown_g",
 
 	GAME_EVENT_DOOR_OPEN: "GAME_EVENT_DOOR_OPEN",
 	GAME_START: "GAME_START",
